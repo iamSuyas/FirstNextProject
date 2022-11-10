18623d4749c875410690f1dbe8e35ed0ac2d8ad0
iamSuyas <103591323+iamSuyas@users.noreply.github.com>
2022-11-10 15:57:07 +0545

    Mostly changes to 3rd and 4th page

--- photoapp/pages/_document.js
+++ photoapp/pages/_document.js
@@ -16,9 +16,6 @@ export default function Document() {
                 <Screenb />
                 <Screenc />
                 <Screend />
-
-
-
                 <NextScript />
             </body>
         </Html>
